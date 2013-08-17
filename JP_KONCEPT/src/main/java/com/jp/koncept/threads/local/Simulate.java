7f507f51df9ecd4f30c47f40537b17af6a892160
dchadha <java.pathshala@gmail.com>
2013-08-18 01:07:34 +0530

    updated Thread Local

--- JP_KONCEPT/src/main/java/com/jp/koncept/threads/local/Simulate.java
+++ JP_KONCEPT/src/main/java/com/jp/koncept/threads/local/Simulate.java
@@ -1,16 +1,12 @@
 /*
- * File: Simulate.java
- * Date: 19-Mar-2013
- *
- * This source code is part of Java Pathshala-Wisdom Being Shared.
- * This program is protected by copyright law but you are authorise to learn 
- * & gain ideas from it. Its unauthorised use is explicitly prohibited & any 
- * addition & removal of material. If want to suggest any changes,
- * you are welcome to provide your comments on GitHub Social Code Area.
- * Its unauthorised use gives Java Pathshala the right to obtain retention orders
- * and to prosecute the authors of any infraction.
- * 
- * Visit us at www.javapathshala.com
+ * File: Simulate.java Date: 19-Mar-2013 This source code is part of Java
+ * Pathshala-Wisdom Being Shared. This program is protected by copyright law but
+ * you are authorise to learn & gain ideas from it. Its unauthorised use is
+ * explicitly prohibited & any addition & removal of material. If want to
+ * suggest any changes, you are welcome to provide your comments on GitHub
+ * Social Code Area. Its unauthorised use gives Java Pathshala the right to
+ * obtain retention orders and to prosecute the authors of any infraction. Visit
+ * us at www.javapathshala.com
  */
 package com.jp.koncept.threads.local;
 
@@ -31,6 +27,9 @@ public class Simulate extends Thread {
 
 		Thread threadTwo = new Simulate();
 		threadTwo.start();
+
+		Thread threadThree = new Simulate();
+		threadThree.start();
 	}
 
 	public void run() {
@@ -38,11 +37,12 @@ public class Simulate extends Thread {
 		TxThreadLocal.set(tx);
 		System.out.println("Thread Awaken.. Good Morning..." + Thread.currentThread().getName());
 		System.out.println("Get Transation details ::");
-		TxService service=new TxService();
+		TxService service = new TxService();
 		service.getTxDetails();
-		System.out.println("Trying to reset the threadlocal");
-		TxThreadLocal.unset();
-		service.getTxDetails();
+
+		// System.out.println("Trying to reset the threadlocal");
+		// TxThreadLocal.unset();
+		// service.getTxDetails();
 	}
 
 	/**
@@ -50,21 +50,22 @@ public class Simulate extends Thread {
 	 */
 	private static synchronized Transaction setTransaction() {
 		Transaction tx = null;
-		//synchronized (Simulate.class) {
-			System.out.println(Thread.currentThread().getName());
-			System.out.println("Entering ()");
-			tx = new Transaction();
-			int nextInt = new Random().nextInt(10);
-			tx.setTxId(nextInt);
-			tx.setTxName("TX-" + nextInt);
-			try {
-				System.out.println("Going to Sleep..." + Thread.currentThread().getName());
-				Thread.sleep(10000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+		// synchronized (Simulate.class) {
+		System.out.println(Thread.currentThread().getName());
+		System.out.println("Entering ()");
+		tx = new Transaction();
+		int nextInt = new Random().nextInt(10);
+		tx.setTxId(nextInt);
+		tx.setTxName("TX-" + nextInt);
+		try {
+			System.out.println("Going to Sleep..." + Thread.currentThread().getName());
+			Thread.sleep(1000);
+			System.out.println("################################");
 
-		//}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		// }
 		return tx;
 	}
 }
