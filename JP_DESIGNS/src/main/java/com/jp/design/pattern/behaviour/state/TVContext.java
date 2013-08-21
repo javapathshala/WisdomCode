3f2e0c2726176a3751d12892ce59832dacbe6839
dchadha <java.pathshala@gmail.com>
2013-08-21 18:15:13 +0530

    State Pattern

---
+++ JP_DESIGNS/src/main/java/com/jp/design/pattern/behaviour/state/TVContext.java
@@ -0,0 +1,33 @@
+/*
+ * File: TVContext.java Date: 21-Aug-2013 This source code is part of Java
+ * Pathshala-Wisdom Being Shared. This program is protected by copyright law but
+ * you are authorise to learn & gain ideas from it. Its unauthorised use is
+ * explicitly prohibited & any addition & removal of material. If want to
+ * suggest any changes, you are welcome to provide your comments on GitHub
+ * Social Code Area. Its unauthorised use gives Java Pathshala the right to
+ * obtain retention orders and to prosecute the authors of any infraction. Visit
+ * us at www.javapathshala.com
+ */
+package com.jp.design.pattern.behaviour.state;
+
+/**
+ * @author dchadha
+ */
+public class TVContext implements State {
+
+	private State tvState;
+
+	public void setState(State state) {
+		this.tvState = state;
+	}
+
+	public State getState() {
+		return this.tvState;
+	}
+
+	@Override
+	public void doAction() {
+		this.tvState.doAction();
+	}
+
+}
---
+++ JP_DESIGNS/src/main/java/com/jp/design/pattern/behaviour/state/TVStartState.java
@@ -0,0 +1,23 @@
+/*
+ * File: TVStartState.java Date: 21-Aug-2013 This source code is part of Java
+ * Pathshala-Wisdom Being Shared. This program is protected by copyright law but
+ * you are authorise to learn & gain ideas from it. Its unauthorised use is
+ * explicitly prohibited & any addition & removal of material. If want to
+ * suggest any changes, you are welcome to provide your comments on GitHub
+ * Social Code Area. Its unauthorised use gives Java Pathshala the right to
+ * obtain retention orders and to prosecute the authors of any infraction. Visit
+ * us at www.javapathshala.com
+ */
+package com.jp.design.pattern.behaviour.state;
+
+/**
+ * @author dchadha
+ */
+public class TVStartState implements State {
+
+	@Override
+	public void doAction() {
+		System.out.println("TV is turned ON");
+	}
+
+}
