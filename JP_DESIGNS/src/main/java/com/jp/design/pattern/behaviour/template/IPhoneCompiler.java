6401607540971850b76d1acce7d3454e88b4b7b3
dchadha <java.pathshala@gmail.com>
2013-08-21 12:49:38 +0530

    Template Pattern

---
+++ JP_DESIGNS/src/main/java/com/jp/design/pattern/behaviour/template/IPhoneCompiler.java
@@ -0,0 +1,30 @@
+/*
+ * File: IPhoneCompiler.java Date: 21-Aug-2013 This source code is part of Java
+ * Pathshala-Wisdom Being Shared. This program is protected by copyright law but
+ * you are authorise to learn & gain ideas from it. Its unauthorised use is
+ * explicitly prohibited & any addition & removal of material. If want to
+ * suggest any changes, you are welcome to provide your comments on GitHub
+ * Social Code Area. Its unauthorised use gives Java Pathshala the right to
+ * obtain retention orders and to prosecute the authors of any infraction. Visit
+ * us at www.javapathshala.com
+ */
+package com.jp.design.pattern.behaviour.template;
+
+/**
+ * @author dchadha
+ */
+public class IPhoneCompiler extends CrossCompiler {
+
+	@Override
+	protected void collectSource() {
+		System.out.println("This is Iphone Source");
+
+	}
+
+	@Override
+	protected void compileToTarget() {
+		System.out.println("This is Iphone Target");
+
+	}
+
+}
